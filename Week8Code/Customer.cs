23c0cb4e007c80ec6ba80ceca19524f308c19a89
BuffTechTalk <79437133+BuffTechTalk@users.noreply.github.com>
2022-10-12 09:17:28 -0500

    Update Customer.cs

--- Week8Code/Customer.cs
+++ Week8Code/Customer.cs
@@ -1,6 +1,6 @@
 class Customer{
-    public static int countCustomer = 0;
-    public string customerName = string.Empty;
+    public static int countCustomer {get;set;} = 0;
+    public string customerName  {get;set;}= string.Empty;
     public Customer(string inputCustomerName){
         customerName = inputCustomerName;
         Console.WriteLine($"Customer {inputCustomerName} created!");
